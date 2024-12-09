74e9891ee48fd47dd04981943c9257ab295e30fa
jhalto <zobayerarmannadim@gmail.com>
2024-12-09 15:27:59 +0600

    time picker added

--- lib/screens/add_task.dart
+++ lib/screens/add_task.dart
@@ -11,26 +11,27 @@ class AddTask extends StatefulWidget {
 }
 
 class _AddTaskState extends State<AddTask> {
-
-
   final TextEditingController _projectController = TextEditingController();
   final TextEditingController _detailController = TextEditingController();
   final TextEditingController _taskController = TextEditingController();
   final FocusNode _focusNodeProject = FocusNode();
   final FocusNode _focusNodeTask = FocusNode();
   final FocusNode _focusNodeDetail = FocusNode();
-  var selectedDate = DateTime.now();
 
-  var focusDate = DateTime.now();
+  DateTime? selectedDate; // Initially null
+  TimeOfDay? selectedTime;
+  DateTime focusDate = DateTime.now();
+  bool _showBody = false;
 
-
-  bool _showBody = false; // Flag to toggle the visibility of the body
-
-
-
-  void _toggleBody() {
+  /// Reset all data
+  void _resetData() {
     setState(() {
-      _showBody = !_showBody; // Toggle the visibility of the body
+      _taskController.clear();
+      _detailController.clear();
+      _projectController.clear();
+      selectedDate = null; // Reset to null
+      selectedTime = null;
+      _showBody = false;
     });
   }
 
@@ -41,7 +42,6 @@ class _AddTaskState extends State<AddTask> {
     _projectController.dispose();
     _detailController.dispose();
     _focusNodeTask.dispose();
-
     super.dispose();
   }
 
@@ -50,20 +50,17 @@ class _AddTaskState extends State<AddTask> {
     return FloatingActionButton(
       onPressed: () {
         showModalBottomSheet(
-          isScrollControlled: true, // Allows the modal to resize for the keyboard
+          isScrollControlled: true,
           context: context,
           builder: (context) {
             return StatefulBuilder(
               builder: (BuildContext context, StateSetter modalSetState) {
-                // Focus the first text field after a short delay
                 Future.delayed(Duration.zero, () {
                   _focusNodeTask.requestFocus();
                 });
                 return Padding(
                   padding: EdgeInsets.only(
-                    left: 16.0,
-                    right: 16.0,
-                    top: 16.0,
+
                     bottom: MediaQuery.of(context).viewInsets.bottom,
                   ),
                   child: Wrap(
@@ -72,32 +69,40 @@ class _AddTaskState extends State<AddTask> {
                         hint: "Task Name",
                         function: () {
                           Future.delayed(Duration.zero, () {
-                             if(_showBody){
-                               _focusNodeDetail.requestFocus();
-                             }else{
-                               Navigator.pop(context);
-                             }
-
-
+                            if (_showBody) {
+                              _focusNodeDetail.requestFocus();
+                            } else {
+                              Navigator.pop(context);
+                            }
                           });
                         },
                         focusNode: _focusNodeTask,
                         textEditingController: _taskController,
                       ),
                       if (_showBody)
-                        BottomForm(function: (){}, focusNode: _focusNodeDetail, textEditingController: _detailController, hint: "Details"),
+                        BottomForm(
+                          function: () {},
+                          focusNode: _focusNodeDetail,
+                          textEditingController: _detailController,
+                          hint: "Details",
+                        ),
+                      Text(
+                        selectedDate != null
+                            ? "${selectedDate!.toLocal().toString().split(' ')[0]} ${selectedTime?.format(context) ?? ''}"
+                            : "",
+                        style: const TextStyle(fontSize: 16),
+                      ),
                       Row(
                         children: [
                           GestureDetector(
-                              onTap: (){
-                                modalSetState(() {
-
-                                  _showBody = !_showBody; // Toggle the visibility of the body
-                                  _focusNodeDetail.requestFocus();
-                                });
-
-                              },
-                              child: Icon(Icons.menu)),
+                            onTap: () {
+                              modalSetState(() {
+                                _showBody = !_showBody;
+                                _focusNodeDetail.requestFocus();
+                              });
+                            },
+                            child: const Icon(Icons.menu),
+                          ),
                           IconButton(
                             onPressed: () {
                               SystemChannels.textInput.invokeMethod('TextInput.hide');
@@ -105,50 +110,66 @@ class _AddTaskState extends State<AddTask> {
                                 context: context,
                                 builder: (context) => Dialog(
                                   child: SingleChildScrollView(
-                                    child: Container(
-                                      height: 350,
-                                      child: TableCalendar(
-                                        focusedDay: DateTime.now(),
-                                        firstDay: DateTime.utc(2024),
-                                        lastDay: DateTime.utc(2030),
-                                        selectedDayPredicate: (day) {
-                                          return isSameDay(selectedDate, day);
-                                        },
-                                        onDaySelected: (selectedDay, focusedDay) {
-                                          modalSetState(() {
-                                            selectedDate = selectedDay;
-                                            focusDate = focusedDay; // update `_focusedDay` here as well
-                                          });
-                                          Navigator.pop(context); // Close the dialog after selection
-                                        },
-                                      ),
+                                    child: Column(
+                                      children: [
+                                        Container(
+                                          height: 350,
+                                          child: TableCalendar(
+                                            focusedDay: focusDate,
+                                            firstDay: DateTime.utc(2024),
+                                            lastDay: DateTime.utc(2030),
+                                            selectedDayPredicate: (day) {
+                                              return isSameDay(selectedDate, day);
+                                            },
+                                            onDaySelected: (selectedDay, focusedDay) {
+                                              modalSetState(() {
+                                                selectedDate = selectedDay;
+                                                focusDate = focusedDay;
+                                              });
+                                              Navigator.pop(context); // Close dialog
+                                            },
+                                          ),
+                                        ),
+                                        IconButton(
+                                          onPressed: () async {
+                                            TimeOfDay? time = await showTimePicker(
+                                              context: context,
+                                              initialTime: TimeOfDay.now(),
+                                            );
+                                            if (time != null) {
+                                              modalSetState(() {
+                                                selectedTime = time;
+                                              });
+                                            }
+                                          },
+                                          icon: const Icon(CupertinoIcons.time),
+                                        ),
+                                      ],
                                     ),
                                   ),
                                 ),
                               );
                             },
-                            icon: Icon(CupertinoIcons.time),
+                            icon: const Icon(CupertinoIcons.time),
                           ),
                           GestureDetector(
                             onTap: () {
-                              modalSetState(() {
-                                _showBody = !_showBody; // Toggle the visibility of the body
-                              });
-                            },
-                            child: Icon(Icons.details),
-                          ),
 
+                            },
+                            child: const Icon(Icons.details),
+                          ),
                         ],
                       ),
+                      const SizedBox(height: 16),
                     ],
                   ),
                 );
               },
             );
           },
-        );
-
-
+        ).whenComplete(() {
+          _resetData();
+        });
       },
       child: const Icon(Icons.add),
     );
@@ -170,42 +191,30 @@ class BottomForm extends StatelessWidget {
   final FocusNode focusNode;
   final TextEditingController textEditingController;
   final String hint;
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(
-         16.0,
-
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(
-            onEditingComplete: function, // Trigger the navigation function
-            focusNode: focusNode,
-
-            controller: textEditingController,
-            decoration: InputDecoration(
-              hintText: hint,
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.transparent
-                )
-              ),
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.transparent)
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.transparent
-                )
-              )
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        TextField(
+          onEditingComplete: function,
+          focusNode: focusNode,
+          controller: textEditingController,
+          decoration: InputDecoration(
+            hintText: hint,
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent),
+            ),
+            border: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent),
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
-
-
